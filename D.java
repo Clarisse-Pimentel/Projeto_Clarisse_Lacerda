1d8b8a685cdb42fe0750d4d5a46e806acecfb242
unknown <clarilpim@gmail.com>
2024-10-29 15:12:51 -0300

    Criacao da Classe D

---
+++ D.java
@@ -0,0 +1,35 @@
+public class D {
+    
+    private String D1;
+    private float D2;
+
+    public D(String d1, int d2) {
+        D1 = d1;
+        D2 = d2;
+    }
+
+    public void setD1(String meuD1) {
+        D1 = meuD1;
+    }
+
+    public void setD2(float meuD2) {
+        D2 = meuD2;
+    }
+
+    public String getD1() {
+        return D1;
+    }
+
+    public float getD2() {
+        return D2;
+    }
+
+    public void MD1() {
+        System.out.println("MD1()");
+    }
+
+    public void MD2() {
+        System.out.println("MD2()");
+    }
+
+}
