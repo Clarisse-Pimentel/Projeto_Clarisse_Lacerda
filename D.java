b8c6084735113d04740c843af818f01ff5e3be6d
Clarisse-Pimentel <clarilpim@gmail.com>
2025-04-17 20:53:57 -0300

    Criacao da classe D

---
+++ D.java
@@ -0,0 +1,30 @@
+public class D {
+
+        private String D1;
+        private float D2;
+    
+        public String getD1() {
+            return D1;
+        }
+    
+        public void setD1(String D1) {
+            this.D1 = D1;
+        }
+    
+        public float getD2() {
+            return D2;
+        }
+    
+        public void setD2(float D2) {
+            this.D2 = D2;
+        }
+    
+        public void MD1() {
+            System.out.println("MD1 chamado");
+        }
+    
+        public void MD2() {
+            System.out.println("MD2 chamado");
+        }
+        
+}
